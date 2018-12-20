107ce28c4e7370de1cdfe23e5453d094328d6a73
Matt Westover <matthew.westover@gmail.com>
2018-12-20 14:59:12 -0700

    finish category and card controller actions;

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,16 +1,46 @@
 class Api::CardsController < ApplicationController
+  before_action :set_category
+  before_action :cards, only: [:show, :update, :destroy]
+
   def index
+    render json: @category.cards
   end
 
   def show
+    render json: @cards
   end
 
   def create
+    card = @category.cards.new(card_params)
+    if card.save
+      render json: card
+    else
+      render json: card.errors
+    end
   end
 
   def update
+    if @card.update(card_params)
+      render json: @card
+    else
+      render json: @card.errors
+    end
   end
 
   def destroy
+    @card.destroy
   end
+
+  private
+    def set_category
+      @category = Category.find(params[:category_id])
+    end
+
+    def set_card
+      @card = @category.posts.find(params[:id])
+    end
+
+    def card_params
+      params.require(:card).permit(:question, :points, :answer1, :answer2, :answer3, :answer4, :correct_answer, :answered)
+    end
 end
--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,16 +1,41 @@
 class Api::CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :update, :destroy]
+
   def index
+    render json: Category.all.order(title: :asc)
   end
 
   def show
+    render json: @category
   end
 
   def create
+    category = Category.create(category_params)
+    if category.save
+      render json: category
+    else
+      render json: category.errors
+    end
   end
 
   def update
+    if @category.update(category_params)
+      render json: @category
+    else
+      render json: @category.errors
+    end
   end
 
   def destroy
+    @category.destroy
   end
+
+  private
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
